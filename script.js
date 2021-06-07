c9ceeb419d963753c26511b5dbbd2259e75c7f34
rusty-reebs <rusty_reebs@tutanota.com>
2021-06-07 17:41:26 +0400

    Added code to build grid and change color with mouse

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 
 const container = document.querySelector(".container");
-
-for (i = 0; i <= 16; i++) {
+function buildGrid () {
+for (i = 0; i <= 256; i++) {
 
 let newSquare = document.createElement("div");
 
 newSquare.classList.add("square");
-newSquare.textContent= "SQUARE";
-
+newSquare.addEventListener("mouseover", function(event) {
+    event.target.style.backgroundColor = "black";
+})
 
 container.appendChild(newSquare);
+
+}
 }
 
+function removeAllChildNodes (parent) {
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function reset () {
+    removeAllChildNodes (container);
+    buildGrid ();
+    console.log ("reset!");
+}
+
+buildGrid ();
+
+// document.getElementById("reset").click = reset ();
+
+let resetMe = document.getElementById("reset");
+// resetMe.addEventListener("onclick", reset ());
+
+
+/* let clickReset = document.getElementById("reset");
+    if (clickReset.addEventListener) clickReset.addEventListener("onclick", removeAllChildNodes (container), false);
+    else if (clickResetTwo.addEventListener) clickResetTwo.addEventListener("onclick", buildGrid ());
+
+*/
