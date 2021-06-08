34db4416dcbe368e0bf53cb0855e01e95ff7f8a6
rusty-reebs <rusty_reebs@tutanota.com>
2021-06-08 17:01:22 +0400

    added prompt and max number of lines

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ Etch-A-Sketch project for theodinproject.com.
 
 What did I learn?
 
+- building a grid with JS and CSS
+- appendChild
+- removeChild
+- event listeners
 
 
 Important dates
 
 June 3, 2021 -- started project
+June 8, 2021 -- finished project, complete with reset button, prompt,
+                and max number of lines
--- script.js
+++ script.js
@@ -1,38 +1,57 @@
 
 const container = document.querySelector(".container");
 function buildGrid () {
-for (i = 0; i <= 256; i++) {
+    for (i = 0; i <= 256; i++) {
+    let newSquare = document.createElement("div");
+    newSquare.classList.add("square");
+    newSquare.addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor = "black";
+        })
 
-let newSquare = document.createElement("div");
+    container.appendChild(newSquare);
 
-newSquare.classList.add("square");
-newSquare.addEventListener("mouseover", function(event) {
-    event.target.style.backgroundColor = "black";
-})
-
-container.appendChild(newSquare);
-
-}
+    }
 }
 
 function removeAllChildNodes (parent) {
-    while(parent.firstChild){
+    while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
 
-function reset () {
+function buildCustomGrid (lines) {
+    for (i = 0; i <= lines*lines; i++) {
+        let newSquare = document.createElement("div");
+        newSquare.classList.add("square");
+        newSquare.addEventListener("mouseover", function(event) {
+            event.target.style.backgroundColor = "black";
+            })
+
+    container.appendChild(newSquare);
+
+    }
+}     
+
+
+function resetNew () {
     removeAllChildNodes (container);
-    buildGrid ();
+    let lines = prompt("How many lines?");
+    while (lines > 100) {
+        alert ("Cannot exceed 100");
+        lines = prompt ("How many lines?");
+    }
+    
+    container.setAttribute("style", `grid-template-columns: repeat(${lines}, 1fr);
+            grid-template-rows: repeat(${lines}, 1fr);`);
+    buildCustomGrid (lines);
     console.log ("reset!");
 }
+ 
+
 
 buildGrid ();
 
 const resetButton = document.getElementById("reset");
-resetButton.addEventListener("click", reset);
-
-
-
+resetButton.addEventListener("click", resetNew);
 
 
